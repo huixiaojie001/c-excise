5131eb642521d6cdd6b3faf7994747653d22e163
huixiaojie001 <18390188508@163.com>
2020-06-29 23:51:07 +0800

    提交训练代码

---
+++ d2/d2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2020;//年份
+            Console.WriteLine(year % 4);//求年份除以4的余数
+        }
+    }
+}
