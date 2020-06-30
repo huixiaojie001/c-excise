a4135dc0105db46b81b8c46a8c3e5ccdbe34be3f
Your Name <18390188508@163.com>
2020-06-30 13:58:24 +0800

    比较运算符

---
+++ d4/d4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3.5;
+            int y = 3;
+            Console.Write(x > y);
+        }
+    }
+}
