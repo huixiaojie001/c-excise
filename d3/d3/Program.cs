86efaa754f875b7551711de8fa026ee65ed6bbe2
huixiaojie001 <18390188508@163.com>
2020-06-30 13:43:54 +0800

    算数运算符自增自减

---
+++ d3/d3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 5;
+            int z = 5;
+            x++;
+            Console.Write(x);
+            Console.Write(++y);
+            Console.Write(++z);
+        }
+    }
+}
