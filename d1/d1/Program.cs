877d79e17b3626efe24ec2fedfcaa1876c5faf8b
huixiaojie001 <18390188508@163.com>
2020-06-30 10:56:32 +0800

    算数运算符加减

--- d1/d1/Program.cs
+++ d1/d1/Program.cs
@@ -11,10 +11,11 @@ namespace d1
         static void Main(string[] args)
         {
             double salary = 6000.00;//基本工资
+            double quanqin = 500.00;
             double prize = 3200.00;//奖金
             double tax = 4500.00;//交税
-            Console.WriteLine("我的工资奖金总额是{0}元", salary + prize);
-            Console.WriteLine("我的税后收入是{0}元", salary + prize - tax);
+            Console.WriteLine("我的工资奖金总额是{0}元", salary + prize + quanqin);
+            Console.WriteLine("我的税后收入是{0}元", salary + prize + quanqin - tax);
         }
     }
 }
