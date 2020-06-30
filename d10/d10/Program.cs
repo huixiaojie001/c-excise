365965f3f7bc4a8de9596958388c92e321ce8f1a
Your Name <18390188508@163.com>
2020-06-30 15:45:48 +0800

    if...else条件结构

---
+++ d10/d10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 17;//年龄
+            if (age > 18)//判断条件，bool类型
+            {//分支1
+                Console.WriteLine("你是成年人");
+            }
+            else
+            {//分支2
+                Console.WriteLine("你是小孩子");
+            }
+        }
+    }
+}
