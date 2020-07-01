0a34dbda910be171183748931424601b8e22c0c0
huixiaojie001 <18390188508@163.com>
2020-07-01 15:32:38 +0800

    do...while循环

---
+++ d21/d21/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 使其程序能输出3个数字
+             */
+            int x = 2;
+            do
+            {
+                x++;
+                Console.WriteLine(x);
+            }
+            while (x > 2 && x <= 4);
+        }
+    }
+}
