7953062be434b1fc869d5444e4970b054328f0b2
huixiaojie001 <18390188508@163.com>
2020-07-01 22:20:30 +0800

    二维数组的声明和访问

---
+++ d35/d35/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 填写 ch 数组的 3 个元素索引，使得程序能够打印出“工程师”。
+             */
+            string[,] ch = { { "我", "是", "一" }, { "名", "工", "程" }, { "师", "啦", "！" } };
+            Console.Write("{0}{1}{2}",ch[1,1],ch[1,2],ch[2,0]);
+        }
+    }
+}
