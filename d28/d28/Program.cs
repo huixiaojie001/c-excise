30391c8d9d8242a11e64b31cd259df69119f6d57
huixiaojie001 <18390188508@163.com>
2020-07-01 20:04:55 +0800

    访问数组元素

---
+++ d28/d28/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 代码声明并初始化一个 string 数组存储“职位”
+               将数组元素初始化为"经理","项目主管","技术总监","财务主管"。
+             */
+            string[] job = new string[] { "经理", "项目主管", "技术总监", "财务总管" };
+            for (int i = 0; i < job.Length; i++)
+            {
+                Console.WriteLine(job[i]);
+            }
+        }
+    }
+}
