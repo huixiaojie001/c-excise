f9d806ad406cc10894dd851ed01d9944e82080cc
huixiaojie001 <18390188508@163.com>
2020-07-01 15:22:06 +0800

    for循环

---
+++ d20/d20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 循环打印 1-12 的整数
+             */
+            for (int x = 1; x < 13; x++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
