ded4d544a3651b818813671a1b6592943d3015af
huixiaojie001 <18390188508@163.com>
2020-07-01 16:34:37 +0800

    嵌套循环编程练习

---
+++ d26/d26/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 嵌套的 for 循环实现图形
+             */
+            for (int x = 1; x <=7; x++)
+            {
+                for (int y = 1; y <= 7; y++)
+                {
+                    /*if (y == x || y+x == 8)
+                    {
+                        Console.Write("O");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }*/
+                    Console.Write(y == x || y + x == 8 ? "O" : ".");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
