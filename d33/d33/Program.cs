42104813b4f256649155129ce8e6eb94fafe6223
huixiaojie001 <18390188508@163.com>
2020-07-01 21:46:29 +0800

    算法查找二编程练习

---
+++ d33/d33/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  使用foreach() 结构，使得程序能够打印数组的所有元素。
+             */
+            string[] t = new string[] { "CD", "ds", "fS", "fr", "WW" };
+            foreach (string x in t)
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
---
+++ d34/d34/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 程序中有一个 int 型数组，包含了若干整数。请检查其中是否有“7的整倍数”
+             */
+            int[] num = { 3, 34, 43, 2, 11, 19, 30, 55, 20 };//声明整数数组，保存一组数据
+            bool figth = false;
+            foreach (int x in num)
+            {
+                if (x % 7 == 0)
+                {
+                    figth = true;
+                }
+            }
+            if (figth)
+            {
+                Console.WriteLine("这组数据中有7的整倍数");
+            }
+            else 
+            {
+                Console.WriteLine("这组数据中没有7的被整数");
+            }
+        }
+    }
+}
