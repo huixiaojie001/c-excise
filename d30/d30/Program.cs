ce17a93d457f51c69f0d0c75cc830e402bcdf79f
huixiaojie001 <18390188508@163.com>
2020-07-01 20:42:47 +0800

    算法-查找

--- d30/d30/Program.cs
+++ d30/d30/Program.cs
@@ -10,6 +10,18 @@ namespace d30
     {
         static void Main(string[] args)
         {
+            /*
+             * 声明数组保存了一些分数，然后循环打印不及格的分数（也就是在数组中查找不及格分数）
+             */
+            int[] score = new int[] { 89, 29, 100, 41, 55, 67, 60 };//分数
+            Console.Write("不及格的有：");
+            for (int x = 0; x <score .Length; x++)
+            {
+                if (score [x]<60)
+                {
+                    Console.Write(score[x]+",");
+                }
+            }
         }
     }
 }
