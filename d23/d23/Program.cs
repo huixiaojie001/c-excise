3426909e7c13c4f8b30bf4c8457314a4d9b0f116
huixiaojie001 <18390188508@163.com>
2020-07-01 15:44:16 +0800

    循环结构之continue

---
+++ d23/d23/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 循环输出 1-9 的整数，请使用 continue 关键字，使得 3 和 8 不会被打印
+             */
+            for (int x = 1; x < 10; x++)//循环语句
+            {
+                if (x == 3 || x == 8)//判断语句
+                {
+                    continue;
+                }
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
