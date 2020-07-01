cf083a2318cf053b94f3ec7c74d5ae93c4d18782
huixiaojie001 <18390188508@163.com>
2020-07-01 16:09:26 +0800

    循环结构之嵌套循环

---
+++ d25/d25/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 使用代码将数字打印成三角形
+             */
+            for (int x = 1; x < 8; x++)
+            {
+                for (int y = 1; y <= x; y++)
+                {
+                    Console.Write(y);
+                }
+                Console.WriteLine();//换行
+            }
+        }
+    }
+}
