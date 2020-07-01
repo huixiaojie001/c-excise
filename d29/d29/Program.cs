d451348d07b599ed8296d4d54632048f3f0f4880
huixiaojie001 <18390188508@163.com>
2020-07-01 20:15:04 +0800

    数组编程练习

---
+++ d29/d29/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 《三国演义》中，蜀国“五虎上将”是：关羽、张飞、赵云、马超、黄忠。
+               请用数组保存五虎上将的名字，然后循环打印。
+             */
+            string[] jiang = new string[] { "关羽", "张飞", "赵云", "马超", "黄忠" };
+                for(int x=0;x<jiang .Length;x++)
+                {
+                    Console.WriteLine(jiang[x]);
+                }
+        }
+    }
+}
