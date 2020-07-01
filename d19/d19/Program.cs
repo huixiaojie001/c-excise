4743b1af2a29acb2e3694d9fa5c86db7268e4ef4
huixiaojie001 <18390188508@163.com>
2020-07-01 14:53:03 +0800

    求和

---
+++ d19/d19/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *求 1-30 的奇数的和 
+             */
+            int x = 1;
+            int sum = 0;//和，初始化为0
+            while(x<=30)//循环条件
+            {
+                if (x % 2 != 0)//筛选条件
+                {
+                    sum += x;
+                }
+                x++;
+            }
+            Console.WriteLine("1-30奇数的和:"+sum);
+        }
+    }
+}
