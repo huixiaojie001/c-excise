39d54d5a7895bfda7d0ff1e976016ee04949e224
huixiaojie001 <18390188508@163.com>
2020-07-01 13:44:50 +0800

    while循环

---
+++ d17/d17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 打印“5 4 3 2 1”
+             */
+            int y = 5;
+            while (y > 0)
+            {
+                Console.Write(y + " ");
+                y--;
+            }
+        }
+    }
+}
