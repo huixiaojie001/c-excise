381a69fa083db1bf79431319de2aa2d0d6b22d72
huixiaojie001 <18390188508@163.com>
2020-07-01 13:50:34 +0800

    while循环编程练习

---
+++ d18/d18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 循环打印 5 次“加油！”
+             */
+            int x;//循环计数变量
+            x=0;//计数变量初始化
+            while(x<5)//循环条件
+            {
+                Console.WriteLine("加油！");
+                x++;//计数变量的自加语句
+            }
+        }
+    }
+}
