e2d65267efd1b078d77b89acb28a8230b17c5c5f
huixiaojie001 <18390188508@163.com>
2020-07-01 11:19:05 +0800

    switch结构

---
+++ d16/d16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 小明发月饼，我们再用 switch 结构发一次
+             */
+            string job = "处长";//职务
+            switch (job)
+            {
+                case "局长": Console.WriteLine("发双黄月饼"); break;
+                case "处长": Console.WriteLine("发蛋黄月饼"); break;
+                case "科长": Console.WriteLine("发枣泥月饼"); break;
+                default: Console.WriteLine("发五仁月饼"); break;
+            }
+        }
+    }
+}
