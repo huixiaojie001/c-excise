6f18d438ff9459cae53ec8af786b13d36ecfef4c
huixiaojie001 <18390188508@163.com>
2020-07-01 15:54:59 +0800

    循环结构之break

---
+++ d24/d24/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 打印 1-5 之间的奇数
+             */
+            for (int x = 1; x <= 5; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("");
+            /*
+             * 输出1-5的整数
+             */
+            int y = 1;
+            while (true)//循环条件永远为true
+            {
+                Console.WriteLine(y);
+                y++;
+                if (y > 5)//判断条件
+                {
+                    break;//结束循环
+                }
+            }
+        }
+    }
+}
