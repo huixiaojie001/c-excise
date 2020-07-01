65976c4d99e5ae0ea275fcf45573f96549eda239
huixiaojie001 <18390188508@163.com>
2020-07-01 15:37:45 +0800

    for循环编程练习

---
+++ d22/d22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 输出六行yeah！
+             */
+            for (int x = 1; x < 7; x++)//循环语句
+            {
+                Console.WriteLine("Yeah!");
+            }
+        }
+    }
+}
