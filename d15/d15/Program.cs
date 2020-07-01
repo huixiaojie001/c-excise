b4313fa7b83509e6537ecf8880f9ebc7f757b5e7
huixiaojie001 <18390188508@163.com>
2020-07-01 11:09:09 +0800

    条件运算符

---
+++ d15/d15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 假设能够被4整除的年份是闰年，其他年份是平年。
+               请编写能判断平年或闰年的代码。
+             */
+            int year = 2012;//年份
+            string text;
+            text = year%4!=0?"平年":"闰年";
+            Console.WriteLine("今年是{0}", text);
+        }
+    }
+}
